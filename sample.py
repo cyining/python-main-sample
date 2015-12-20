0c5354dd9f2302efdd186fb0150171cc1c5dc012
Cherng-Ying Ing <cying.ing@gmail.com>
2015-12-20 19:16:44 +0800

    Add doc string as help document
    Modify the doc string to suit your needs:
    - 1st line: Description of this python application
    - 2nd line: Creation date and author info
    
    Show help document automatically when there is no arguments

--- sample.py
+++ sample.py
@@ -1,6 +1,22 @@
 #!/usr/bin/env python
+"""Python sample application
+2015'12/20 by Cherng-Ying Ing <cying.ing@gmail.com>
+Usage: %s [options]
+Options:
+"""
 
 def main(argv):
+	from getopt import getopt
+	sopts = ""
+	opts, args = getopt(argv[1:], sopts)
+	show_help = False
+	if not args:
+		show_help = True
+	for k, v in opts:
+		pass
+	if show_help:
+		print(__doc__ % argv[0])
+		return 1
 	return 0
 
 if __name__ == "__main__":
