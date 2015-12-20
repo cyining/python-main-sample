c44c02feb0853c4d4efd3e6fd7b996e1efa642b6
Cherng-Ying Ing <cying.ing@gmail.com>
2015-12-20 19:53:48 +0800

    Add sample function that output a file with tabs to spaces option
    Demonstrate how to:
    - Modify the usage line
    - Add an option with value
    - Check option value
    - Work with arguments
    
    The following will generate test.py with 4 spaces for each tab stop:
    
    python sample.py -t 4 test

--- sample.py
+++ sample.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 """Python sample application
 2015'12/20 by Cherng-Ying Ing <cying.ing@gmail.com>
-Usage: %s [options]
+Usage: %s [options] <output-file-name>
 Options:
 """
 
@@ -9,14 +9,21 @@ def main(argv):
 	from getopt import getopt
 	topts = []
 	topts += [("h", "Show help")]
+	topts += [("t:", "Output each tab as <n> spaces")]
 	sopts = "".join(x for x, _ in topts)
 	opts, args = getopt(argv[1:], sopts)
 	show_help = False
+	tab_space = 0
 	if not args:
 		show_help = True
 	for k, v in opts:
 		if k == "-h":
 			show_help = True
+		elif k == "-t":
+			tab_space = int(v)
+			if tab_space not in range(9):
+				print("-t accept values between 1~8")
+				return 2
 	if show_help:
 		def _help(sopt, text):
 			v = ""
@@ -28,6 +35,22 @@ def main(argv):
 			return "\t-" + sopt[0] + v + "\t" + text + "\n"
 		print(__doc__ % argv[0] + "".join(_help(*x) for x in topts))
 		return 1
+	ofname = args[0]
+	ifname = argv[0]
+	if "." not in ofname or ofname[-3:] != ".py":
+		ofname += ".py"
+	from os.path import basename
+	if ofname.lower() == basename(ifname).lower():
+		print("Only accept file name other than %s" % ifname)
+		return 2
+	ifp = open(ifname, "rt")
+	ofp = open(ofname, "wt")
+	for line in ifp:
+		if tab_space:
+			line = line.replace("\t", " " * tab_space)
+		ofp.write(line)
+	ifp.close()
+	ofp.close()
 	return 0
 
 if __name__ == "__main__":
