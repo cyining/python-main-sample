968ff03d687d7bef19c9930e40709c1c3d27e6da
Cherng-Ying Ing <cying.ing@gmail.com>
2015-12-22 20:02:15 +0800

    Add arguments with input file and optional output file
    Demonstrate how to:
    - Modify the usage line
    - Work with file name arguments
    - Append file name suffix and extension

--- sample.py
+++ sample.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python
 """Python sample application
 2015'12/20 by Cherng-Ying Ing <cying.ing@gmail.com>
-Usage: %s [options]
+Usage: %s [options] <input-file-name> [output-file-name]
 Options:
 """
+import os.path
+
+def frame_suffix(fname, suffix="", ext=".py"):
+	fbase, fext = os.path.splitext(fname)
+	if not fext or fext.lower() != ext:
+		fext += ext
+	return fbase + suffix + fext
 
 def main(argv):
 	from getopt import getopt
@@ -34,6 +41,17 @@ def main(argv):
 			return "\t-" + sopt[0] + v + "\t" + text + "\n"
 		print(__doc__ % argv[0] + "".join(_help(*x) for x in topts))
 		return 1
+	ifname = frame_suffix(args[0])
+	if not os.path.exists(ifname):
+		print("Input file not found: %s" % ifname)
+		return 2
+	if len(args) > 1:
+		ofname = frame_suffix(args[1])
+	else:
+		ofname = frame_suffix(ifname, "_o")
+	if ifname.lower() == ofname.lower():
+		print("Input and output files must be different")
+		return 2
 	return 0
 
 if __name__ == "__main__":
