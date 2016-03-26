00d2db105106eefd1567f7698688c5db1808b428
Cherng-Ying Ing <cying.ing@gmail.com>
2016-03-27 03:22:39 +0800

    Add function "Text line ending reformat tool"

--- reline.py
+++ reline.py
@@ -1,11 +1,30 @@
 #!/usr/bin/env python
-"""Python sample application
+"""Text line ending reformat tool
 2015'12/20 by Cherng-Ying Ing <cying.ing@gmail.com>
 Usage: %s [options] <input-file-name> [output-file-name]
 Options:
 """
 import os
 import os.path
+
+def tail_cut(line, tail_chrs):
+	"""
+	Cut a line from where the leading part have no tailing characters
+
+	- line: input line string
+	- tail_chrs: tailing characters
+
+	Return a tuple containing the leading and tailing parts
+	"""
+	size = len(line)
+	for i in range(size-1, -1, -1):
+		if line[i] not in tail_chrs:
+			cut = i + 1
+			return line[:cut], line[cut:]
+	return "", line
+
+crlf_chrs = "\r\n"
+tail_chrs = " \t"
 
 def frame_suffix(fname, suffix="", ext=""):
 	fbase, fext = os.path.splitext(fname)
@@ -17,6 +36,8 @@ def main(argv):
 	from getopt import getopt
 	topts = []
 	topts += [("h", "Show help")]
+	topts += [("t", "Strip tailing white-space (False)")]
+	topts += [("d", "Apply DOS style line ending (False)")]
 	topts += [("o", "Generate output file (False)")]
 	topts += [("i:", "Convert inplace and move original to <d:e>")]
 	sopts = "".join(x for x, _ in topts)
@@ -28,6 +49,8 @@ def main(argv):
 				if l >= 0 and l < r:
 					return val_type(text[l+1:r])
 	show_help = False
+	strip_tail = _init("t")
+	dos_style = _init("d")
 	output = _init("o")
 	move_dir, move_ext = None, None
 	if not args:
@@ -35,6 +58,10 @@ def main(argv):
 	for k, v in opts:
 		if k == "-h":
 			show_help = True
+		elif k == "-t":
+			strip_tail = True
+		elif k == "-d":
+			dos_style = True
 		elif k == "-o":
 			output = True
 		elif k == "-i":
@@ -72,15 +99,48 @@ def main(argv):
 	if ifname.lower() == ofname.lower():
 		print("Input and output files must be different")
 		return 2
+	ending = "\n"
+	if dos_style:
+		ending = "\r\n"
+	counts = {"\n": 0, "\r\n": 0}
+	std_counts = counts.keys()
+	tail_count = 0
 	ifp = open(ifname, "rt")
 	if output:
 		ofp = open(ofname, "wt")
 	for line in ifp:
+		text, crlf = tail_cut(line, crlf_chrs)
+		#print(repr((text, crlf)))
+		if crlf not in counts:
+			counts[crlf] = 0
+		counts[crlf] += 1
+		head, tail = tail_cut(text, tail_chrs)
+		if tail:
+			#print(repr((text, tail, crlf)))
+			tail_count += 1
 		if output:
+			line = text
+			if strip_tail:
+				line = head
+			line += ending
 			ofp.write(line)
 	ifp.close()
 	if output:
 		ofp.close()
+	totals = []
+	for k in std_counts:
+		v = counts[k]
+		if v:
+			totals.append('%s:%d' % (repr(k), v))
+	odd = len(counts) - len(std_counts)
+	if odd:
+		totals.append('odd:%d' % odd)
+	for k, v in counts.items():
+		if k not in std_counts:
+			totals.append('%s:%d' % (repr(k), v))
+	if tail_count:
+		totals.append('tail:%d' % tail_count)
+	print('%s %s' % (ifname, ' '.join(totals)))
 	if move_dir or move_ext:
 		nfname = ifname
 		if move_dir:
